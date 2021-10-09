b5fac5ffd4cc285016ea466b7dbe6173bcd72245
Laura Medina <lcmedina390@gmail.com>
2021-10-09 08:12:08 -0500

    fixed time display

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,16 @@
 function updateClock() {
     let today = new Date();
-    let time = today.getHours() + ":" + today.getMinutes();
+    let min = today.getMinutes();
+    if (min < 10) {
+        min = `0${min}`;
+    }
+    let hour = today.getHours();
+    let ampm = "AM";
+    if (hour > 12) {
+        hour -= 12;
+        ampm = "PM";
+    }
+    let time = `${hour}:${min} ${ampm}`;
     document.getElementById('displayTime').innerHTML = time;
 }
 setInterval(updateClock, 1000);
